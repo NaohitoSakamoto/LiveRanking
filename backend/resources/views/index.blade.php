10bd3065bdb35e8a6b9e38b986905daa35bb303e
NaohitoSakamoto <naohitogithub@gmail.com>
2020-11-02 04:32:36 -0800

    heroku deploy view edit

--- backend/resources/views/index.blade.php
+++ backend/resources/views/index.blade.php
@@ -5,11 +5,13 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>生放送ランキング</title>
 	<link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
-        <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <link href="../css/app.css" rel="stylesheet">
+        {{-- <link href="{{ asset('/css/app.css') }}" rel="stylesheet"> --}}
     </head>
     <body>
         <h1><p>Youtube Live Ranking</p></h1>
         <div id="react-display-ranking"></div>
-        <script src="{{ asset('js/app.js')}}"></script>
+        <script src="../js/app.js"></script>
+        {{-- <script src="{{ asset('js/app.js')}}"></script> --}}
     </body>
 </html>
